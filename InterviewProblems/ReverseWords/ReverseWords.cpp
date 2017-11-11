e98ec9e5c30db7f65ae3bb53c5e5d128494df6d2
jramell <sergiomaderarangel@gmail.com>
2017-11-10 22:41:31 -0500

    added Reverse Words solution

---
+++ InterviewProblems/ReverseWords/ReverseWords.cpp
@@ -0,0 +1,21 @@
+#include "ReverseWords.h"
+void jr::reverseWords(string& sentence) {
+    string answer = "";
+    for(int i = sentence.size() - 1; i > 0; i--) {
+        if(sentence.at(i) == ' ') {
+            if(!answer.empty()) {
+                answer += " ";
+            }
+            for(int j = i+1; j < sentence.size() && sentence.at(j) != ' '; j++) {
+                answer += sentence.at(j);
+            }
+        }
+    }
+    if(!answer.empty()) {
+        answer += " ";
+    }
+    for(int i = 0; i < sentence.size() && sentence.at(i) != ' '; i++) {
+        answer += sentence.at(i);
+    }
+    sentence = answer;
+}
---
+++ InterviewProblems/ReverseWords/ReverseWords.h
@@ -0,0 +1,21 @@
+//
+// Created by Sergio on 11/10/2017.
+//
+
+#ifndef SERGIOTEST_REVERSEWORDS_H
+#define SERGIOTEST_REVERSEWORDS_H
+
+namespace jr {
+    /**
+     * Original problem: https://www.interviewbit.com/problems/reverse-the-string/
+     * Given an input string, reverse the string word by word.
+     * Example:
+     *      Given sentence = "the sky is blue"
+     *      return "blue is sky the"
+     * @param sentence group of words that must be reversed
+     */
+    void reverseWords(string& sentence);
+}
+
+
+#endif //SERGIOTEST_REVERSEWORDS_H
